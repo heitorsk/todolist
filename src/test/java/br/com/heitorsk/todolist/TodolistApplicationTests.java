3c812429def761617ef9f983fed4b60062ba1c5c
heitorsk <skheitor3@gmail.com>
2023-10-15 14:01:12 -0300

    first commit

---
+++ src/test/java/br/com/heitorsk/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.heitorsk.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
